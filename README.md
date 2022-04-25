31083f45531dc7c03826f7bc9f80288fdf84afb5
Alexandra Sabatin <s4batin@gmail.com>
2022-04-25 03:32:17 +0300

    Create gallery markup and open/close image

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # goit-js-hw-07
+
 JS homework #7
+
+Task 1 - Image gallery
+Create a gallery with the ability to click on its items and view full size images in a modal window. Adding the script and styles of the modal window library basicLightbox.
+
+Task 2 - SimpleLightbox library
+Create the same gallery as in the first task, but using the SimpleLightbox library, which will handle image clicks, modal opening and closing and image scrolling with the keyboard.
--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', openImgHandler);
+
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>`
+    }).join('');
+};
+
+function openImgHandler(e) {
+    const isImageLink = e.target.classList.contains('gallery__image');
+    if (!isImageLink) {
+        return;
+    };
+
+    e.preventDefault();
+
+    const keyboardCloseImgHandler = e => {
+        if (e.code === 'Escape') {
+            lightbox.close();
+        }
+    };
+
+    const lightbox = basicLightbox.create(
+        `<img src="${e.target.dataset.source}" alt="${e.target.alt}" />`,
+        {
+            onShow: (lightbox) =>
+                window.addEventListener("keydown", keyboardCloseImgHandler),
+            onClose: (lightbox) =>
+                window.removeEventListener("keydown", keyboardCloseImgHandler),
+        }
+    );
+    lightbox.show();
+};
+
+// console.log(galleryItems);
