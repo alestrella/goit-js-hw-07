128990023817201d1f9eb137ea075d879cf5ec60
Alexandra Sabatin <s4batin@gmail.com>
2022-04-25 16:57:29 +0300

    Create gallery using the Simple Lightbox library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', openImgHandler);
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+        }).
+        join('');
+};
+
+function openImgHandler(e) {
+    const isImageLink = e.target.classList.contains('gallery__image');
+    if (!isImageLink) {
+        return;
+    };
+    
+    e.preventDefault();
+
+    let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+}
+
+// console.log(galleryItems);
