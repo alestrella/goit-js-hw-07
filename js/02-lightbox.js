8ecc3b3385ea767db6a9d06d8c80f74ea158afa7
Alexandra Sabatin <s4batin@gmail.com>
2022-04-25 17:52:10 +0300

    Add minor changes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// console.log(galleryItems);
+
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryMarkup(galleryItems);
 
@@ -19,14 +21,8 @@ function createGalleryMarkup(items) {
 };
 
 function openImgHandler(e) {
-    const isImageLink = e.target.classList.contains('gallery__image');
-    if (!isImageLink) {
-        return;
-    };
-    
     e.preventDefault();
 
-    let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-}
-
-// console.log(galleryItems);
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt', captionDelay: 250, docClose: false});
+};
